8a8591a8f6ae952efec551cc8634a1449fdf12c2
cybrkit <cybrkit@gmail.com>
2020-02-19 13:10:13 +1100

    Tested draft 1 on 1 master, 2 nodes, no network pod.

--- scripts/k8s_init.sh
+++ scripts/k8s_init.sh
@@ -8,16 +8,16 @@
 
 sudo apt update
 while sudo fuser /var/{lib/{dpkg,apt/lists},cache/apt/archives}/lock >/dev/null 2>&1; do
+  echo "Waiting for APT..."
   sleep 5
 done
 
 sudo apt-mark unhold python3 python3-pip
-sudo apt install -y python3=3.6.7-1~18.04 python3-pip=9.0.1-2.3~ubuntu1.18.04.1 curl apt-transport-https git
+sudo apt install -y python3=3.6.7-1~18.04 python3-pip=9.0.1-2.3~ubuntu1.18.04.1 curl apt-transport-https git sshpass
 sudo apt-mark hold python3 python3-pip
 
 pip3 install ansible==2.9.5
-/usr/local/bin/ansible --version
-/usr/local/bin/ansible-playbook --version
 
-git clone https://github.com/cybrkit/k8s.git k8s_labs
-/usr/local/bin/ansible-playbook ./k8s_labs/playbooks/installation/main.yaml -i inventory.ini
+export ANSIBLE_HOST_KEY_CHECKING=False
+export ANSIBLE_REMOTE_TMP=/tmp/k8s_lab
+/usr/local/bin/ansible-playbook ../playbooks/installation/main.yaml -i inventory.ini
